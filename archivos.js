fbb27c0b36450dc2b6db9a98ecc5b71a5c3ffa7e
juan175 <gonzlezjuandaniel@gmail.com>
2021-10-08 10:13:14 -0300

    clase 2 js

---
+++ archivos.js
@@ -0,0 +1,153 @@
+
+
+let nombre =prompt("ingrese su Nombre");
+let dia =prompt("ingrese su dia de nacimiento");
+let mes =prompt("ingrese el mes de nacimiento");
+
+
+
+
+
+if (mes == 1 || mes == "enero"){
+    if(dia >= 21){
+    console.log("sr/a. " + nombre + " usted es de ACUARIO");
+    alert("sr/a. " + nombre + " usted es de ACUARIO");
+     }
+    else
+     {
+      console.log("sr. " + nombre + " usted es de CAPRICORNIO");
+      alert("sr/a. " + nombre + " usted es de CAPRICORNIO");
+     }
+}
+
+if (mes == 2 || mes == "febrero"){
+  if(dia >= 19){
+  console.log("sr. " + nombre + " usted es de PISCIS");
+  alert("sr/a. " + nombre + " usted es de PISCIS");
+   }
+  else
+   {
+    console.log("sr. " + nombre + " usted es de ACUARIO");
+    alert("sr/a. " + nombre + " usted es de ACUARIO");
+   }
+}
+
+if (mes == 3 || mes == "marzo"){
+  if(dia >= 21){
+  console.log("sr. " + nombre + " usted es de ARIES");
+  alert("sr/a. " + nombre + " usted es de ARIES");
+   }
+  else
+   {
+    console.log("sr. " + nombre + " usted es de PISCIS");
+    alert("sr/a. " + nombre + " usted es de PISCIS");
+   }
+}
+
+if (mes == 4 || mes == "abril"){
+  if(dia >= 21){
+  console.log("sr. " + nombre + " usted es de TAURO");
+  alert("sr/a. " + nombre + " usted es de TAURO");
+   }
+  else
+   {
+    console.log("sr. " + nombre + " usted es de ARIES");
+    alert("sr/a. " + nombre + " usted es de ARIES");
+   }
+}
+
+if (mes == 5 || mes == "mayo"){
+  if(dia >= 22){
+  console.log("sr. " + nombre + " usted es de GEMINIS");
+  alert("sr/a. " + nombre + " usted es de GEMINIS");
+   }
+  else
+   {
+    console.log("sr. " + nombre + " usted es de TAURO");
+    alert("sr/a. " + nombre + " usted es de TAURO");
+   }
+}
+
+if (mes == 6 || mes == "junio"){
+  if(dia >= 22){
+  console.log("sr. " + nombre + " usted es de CANCER");
+  alert("sr/a. " + nombre + " usted es de CANCER");
+   }
+  else
+   {
+    console.log("sr. " + nombre + " usted es de GEMINIS");
+    alert("sr/a. " + nombre + " usted es de GEMINIS");
+   }
+}
+
+if (mes == 7 || mes == "julio"){
+  if(dia >= 23){
+  console.log("sr. " + nombre + " usted es de LEO");
+  alert("sr/a. " + nombre + " usted es de LEO");
+   }
+  else
+   {
+    console.log("sr. " + nombre + " usted es de CANCER");
+    alert("sr/a. " + nombre + " usted es de CANCER");
+   }
+}
+
+if (mes == 8 || mes == "agosto"){
+  if(dia >= 24){
+  console.log("sr. " + nombre + " usted es de VIRGO");
+  alert("sr/a. " + nombre + " usted es de VIRGO");
+   }
+  else
+   {
+    console.log("sr. " + nombre + " usted es de LEO");
+    alert("sr/a. " + nombre + " usted es de LEO");
+   }
+}
+
+if (mes == 9 || mes == "septiembre"){
+  if(dia >= 24){
+  console.log("sr. " + nombre + " usted es de LIBRA");
+  alert("sr/a. " + nombre + " usted es de LIBRA");
+   }
+  else
+   {
+    console.log("sr. " + nombre + " usted es de VIRGO");
+    alert("sr/a. " + nombre + " usted es de VIRGO");
+   }
+}
+
+if (mes == 10 || mes == "octubre"){
+  if(dia >= 24){
+  console.log("sr. " + nombre + " usted es de ESCORPIO");
+  alert("sr/a. " + nombre + " usted es de ESCORPIO");
+   }
+  else
+   {
+    console.log("sr. " + nombre + " usted es de LIBRA");
+    alert("sr/a. " + nombre + " usted es de LIBRA");
+   }
+}
+
+if (mes == 11 || mes == "noviembre"){
+  if(dia >= 23){
+  console.log("sr. " + nombre + " usted es de SAGITARIO");
+  alert("sr/a. " + nombre + " usted es de SAGITARIO");
+   }
+  else
+   {
+    console.log("sr. " + nombre + " usted es de ESCORPIO");
+    alert("sr/a. " + nombre + " usted es de ESCORPIO");
+   }
+}
+
+if (mes == 12 || mes == "diciembre"){
+  if(dia >= 22){
+  console.log("sr. " + nombre + " usted es de CAPRICORNIO");
+  alert("sr/a. " + nombre + " usted es de CAPRICORNIO");
+   }
+  else
+   {
+    console.log("sr. " + nombre + " usted es de SAGITARIO");
+    alert("sr/a. " + nombre + " usted es de SAGITARIO");
+   }
+}
